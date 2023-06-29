821e6af03e6c9ad2678f64d6e74063226de33b85
mike <mjowiti@gmail.com>
2023-06-29 18:07:23 +0300

    completed

--- index.js
+++ index.js
@@ -1,34 +1,73 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
+  const seenNumber = {};
+ 
+  for (const number of array) {
+   const complement =target - number;
+   if (complement in seenNumbers) return true;
+   seenNumbers[numbers] = true;
+  }
+  return false;
+ }
+ function hasTargetSum(array, target) {
+   for (let i=0; i< array.length; i++){
+     const complement = target -array[i];
+     for(let j = i+1; j< array.length; j++){
+       if (array[j] === complement) return true;
+     }
+   }
+   return false;
+ }
+ 
+ 
+ 
+ function findSock(array) {
+   for (const item of array) {
+     if (item === "sock") return "sock";
+   }
+ }
+ 
+ function findSock(object){
+   if (object.sock) return "sock";
+ }
+ /* 
+   Write the Big O time complexity of your function here
+ */
+ 
+ /* 
+   Add your pseudocode here
+ */
+ 
+ /*
+   Add written explanation of your solution here
+ */
+ 
+ // You can run `node index.js` to view these console logs
+ if (require.main === module) {
+   // add your own custom tests in here
+   console.log("Expecting: true");
+   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+ 
+   console.log("");
+ 
+   console.log("Expecting: true");
+   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+ 
+   console.log("");
+ 
+   console.log("Expecting: false");
+   console.log("=>", hasTargetSum([1, 2, 5], 4));
+   
+   console.log ("");
+ 
+   console.log("Expecting: false");
+   console.log("=>", hasTargetSum([4], 4));
+ 
+   console.log("");
+ 
+   console.log("Expecting: true");
+   console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
+ 
+ 
+ }
+ 
+ module.exports = hasTargetSum;
